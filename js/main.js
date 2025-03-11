23c16f37f37f836bc216eba6b7a9e9dd64d2f0e0
Rashed <ri.rashed33@gmail.com>
2025-03-11 18:37:55 +0600

    header completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector(".hamburger").addEventListener("click", function () {
+        document.body.classList.toggle("menu-active");
+    });
+
     let menuItems = document.querySelectorAll("li.menu-item-has-children > a");
     menuItems.forEach(item => {
+        item.addEventListener("click", function () {
+            this.closest('li.menu-item-has-children').classList.add("show");
+        });
+
         item.addEventListener("mouseenter", function () {
             this.closest('li').classList.add("active");
         });
@@ -11,7 +19,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     });
 
-    document.querySelector(".hamburger").addEventListener("click", function () {
-        document.body.classList.toggle("menu-active");
-    });
+    let backBtns = document.querySelectorAll(".back-btn");
+    menuItems.forEach(item => {
+        item.addEventListener("click", function () {
+            this.closest('li.menu-item-has-children').classList.remove("show");
+        });
+    })
+
 });
