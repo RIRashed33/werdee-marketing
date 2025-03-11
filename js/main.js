36e117b26ce4008b8c5c2d237a3a8fcd79f6ed0f
Rashed <ri.rashed33@gmail.com>
2025-03-12 00:03:44 +0600

    header completed 2

--- js/main.js
+++ js/main.js
@@ -3,27 +3,56 @@ document.addEventListener("DOMContentLoaded", function () {
         document.body.classList.toggle("menu-active");
     });
 
+    function addHoverEvent(event) {
+        let item = event.currentTarget;
+        item.closest("li").classList.add("active");
+    }
+    
+    function removeHoverEvent(event) {
+        let item = event.currentTarget;
+        item.closest("li").classList.remove("active");
+    }
+    
+    function addClickEvent(event) {
+        event.preventDefault();
+        let item = event.currentTarget;
+        item.closest("li.menu-item-has-children").classList.toggle("active");
+    }
+    
     let menuItems = document.querySelectorAll("li.menu-item-has-children > a");
-    menuItems.forEach(item => {
-        item.addEventListener("click", function () {
-            this.closest('li.menu-item-has-children').classList.add("show");
+    function resizeHandleEvents() {
+        let windowWidth = window.innerWidth;
+        menuItems.forEach(item => {
+            let parentLi = item.closest("li.menu-item-has-children");
+    
+            item.removeEventListener("click", addClickEvent);
+            item.removeEventListener("mouseenter", addHoverEvent);
+            parentLi.removeEventListener("mouseleave", removeHoverEvent);
+    
+            if (windowWidth > 991) {
+                item.addEventListener("mouseenter", addHoverEvent);
+                parentLi.addEventListener("mouseleave", removeHoverEvent);
+            } else {
+                item.addEventListener("click", addClickEvent);
+            }
         });
+    }
+    resizeHandleEvents();
 
-        item.addEventListener("mouseenter", function () {
-            this.closest('li').classList.add("active");
-        });
-
-        item.closest('li').addEventListener("mouseleave", function () {
-            this.classList.remove("active");
-        });
-
+    let lastWindowWidth = window.innerWidth;
+    window.addEventListener("resize", function () {
+        let currentWindowWidth = window.innerWidth;
+        
+        if ((lastWindowWidth > 991 && currentWindowWidth < 992) || (lastWindowWidth < 992 && currentWindowWidth > 991)) {
+            lastWindowWidth = currentWindowWidth;
+            resizeHandleEvents();
+        }
     });
 
+
     let backBtns = document.querySelectorAll(".back-btn");
-    menuItems.forEach(item => {
-        item.addEventListener("click", function () {
-            this.closest('li.menu-item-has-children').classList.remove("show");
-        });
+    backBtns.forEach(item => {
+        item.addEventListener("click", addClickEvent);
     })
 
 });
